6747410f57611092d5efd101321409a5fcdda7ae
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2014-08-09 22:59:11 +0300

    Fix tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -19,8 +19,8 @@ object ApplicationSpec extends PlaySpecification {
     initiliazeTestData
   }
 
-  def checkFolders(folder: String): String = {
-    val url = s"http://localhost:9000/mails/${folder}"
+  def checkFolder(port: Int, folder: String): String = {
+    val url = s"http://localhost:$port/mails/${folder}"
     val response = await(WS.url(url).withHeaders(CONTENT_TYPE -> "application/json", ACCEPT -> "application/json").get())
     response.status must equalTo(OK)
     val results = response.json.as[JsArray].value
@@ -29,15 +29,15 @@ object ApplicationSpec extends PlaySpecification {
     results(0) \ "contact" must beAnInstanceOf[JsString]
     results(0) \ "subject" must beAnInstanceOf[JsString]
     results(0) \ "message" must beAnInstanceOf[JsString]
-    (results(0) \ "id").toString().stripPrefix("\"").stripSuffix("\"")
+    (results(0) \ "id").as[String]
   }
 
   "test list folders" in new WithServer {
-    Seq("inbox","later","trash","sent") map checkFolders
+    Seq("inbox","later","trash","sent") map(checkFolder(port, _))
   }
 
   "test create mail" in new WithServer {
-    val url = "http://localhost:9000/mails"
+    val url = s"http://localhost:$port/mails"
     val json = Json.obj("contact" -> "testContact", "subject" -> "testSubject", "message" -> "testMessage", "folder" -> "sent")
     val response = await(WS.url(url).withHeaders(CONTENT_TYPE -> "application/json", ACCEPT -> "application/json").post(json))
     response.status must equalTo(OK)
@@ -45,16 +45,16 @@ object ApplicationSpec extends PlaySpecification {
   }
 
   "test update mail" in new WithServer {
-    val testId = checkFolders("inbox")
-    val url = s"http://localhost:9000/mails/sent/${testId}"
+    val testId = checkFolder(port, "inbox")
+    val url = s"http://localhost:$port/mails/sent/${testId}"
     val response = await(WS.url(url).withHeaders(CONTENT_TYPE -> "application/json", ACCEPT -> "application/json").get())
     response.status must equalTo(OK)
     response.json \ "status" must beEqualTo(JsString("OK"))
   }
 
   "test delete mail" in new WithServer {
-    val testId = checkFolders("later")
-    val url = s"http://localhost:9000/mails/${testId}"
+    val testId = checkFolder(port, "later")
+    val url = s"http://localhost:$port/mails/${testId}"
     val response = await(WS.url(url).delete())
     response.status must equalTo(OK)
     response.json \ "status" must beEqualTo(JsString("OK"))
